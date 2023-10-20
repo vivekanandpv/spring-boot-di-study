e212a114ddc8001f56655aa6af9dcefd689677ea
Vivekanand P V <vivekanandpv@outlook.com>
2023-10-20 13:45:53 +0530

    initial draft

---
+++ src/test/java/com/vivekanandpv/springbootdistudy/SpringBootDiStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivekanandpv.springbootdistudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDiStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
